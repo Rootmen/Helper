4a4244e4068d063235c8b604b2cb10c3e1cda986
MainHome <10poma10@mail.ru>
2019-02-05 17:47:51 +0300

    Исправление мелких недочетов и удаление ненужных функций
     И добавление комментариев

--- Helper/db_access/Main.py
+++ Helper/db_access/Main.py
@@ -7,6 +7,7 @@ from xlsxwriter import Workbook
 
 GenderMass = ["студенки", "студента"]
 
+# Функция создания базы данных, запускать только один раз при ее создании
 def First_Init():
     conn = sqlite3.connect(os.path.abspath("Helper\db_access\db_s.db"))
     cursor = conn.cursor()
@@ -24,7 +25,7 @@ def First_Init():
     conn.close()
     return
 
-
+# Функция добавления данных в БД, если такой записи нету
 def Insert_Data(gender, group, surname, name, lastname, number, typeconcession):
     gender = GenderMass[int(gender)]
     conn = sqlite3.connect(os.path.abspath("Helper\db_access\db_s.db"))
@@ -41,34 +42,7 @@ def Insert_Data(gender, group, surname, name, lastname, number, typeconcession):
     conn.close()
     return
 
-
-def Get_Data():
-    conn = sqlite3.connect(os.path.abspath("Helper\db_access\db_s.db"))
-    c = conn.cursor()
-    workbook = Workbook('db_accel.xlsx')
-    worksheet = workbook.add_worksheet()
-    c.execute("select * from comments")
-    mysel = c.execute("select * from comments ")
-    for i, row in enumerate(mysel):
-        for j, value in enumerate(row):
-            worksheet.write(i, j, row[j])
-    workbook.close()
-    conn.close()
-    File_Path = os.path.abspath('db_accel.xlsx')
-    fp = open(File_Path, "rb")
-    response = HttpResponse(fp.read())
-    fp.close()
-    file_type = mimetypes.guess_type(File_Path)
-    if file_type is None:
-        file_type = 'application/octet-stream'
-    response['Content-Type'] = file_type
-    response['Content-Length'] = str(os.stat(File_Path).st_size)
-    response['Content-Disposition'] = "attachment; filename=Spisok_Podavshix.xlsx"
-
-    # Чистка временнойго файла
-    os.remove(File_Path)
-    return response
-
+# Функция генерации экселя по данным из БД
 def Get_Data():
     conn = sqlite3.connect(os.path.abspath("Helper\db_access\db_s.db"))
     c = conn.cursor()
--- Helper/mainlog.py
+++ Helper/mainlog.py
@@ -43,31 +43,29 @@ def Set_Data(request):
 # Рендер главной страницы
 @validate_captcha
 def Index(request):
-    context = {
-        'GOOGLE_RECAPTCHA_SITE_KEY': settings.GOOGLE_RECAPTCHA_SITE_KEY,
-    }
+    return render(request, '123.html')
 
-    return render(request, '123.html', context)
-
-# Рендер главной страницы
+# Перенапрвление на главную страницу
 def Any_Page(request):
     return redirect("/index")
 
-# Обработка выдачи данных
+# Запрос на создание ссылки для загрузки
 def Get_Data(request):
     (Patch,Patch_Cript) = Get_Path()
     global Patch_Of_Get
     Patch_Of_Get = Patch
     print("Генерация динамической ссылки для загрузки")
-    threading.Timer(1, Clear_Page).start()
+    threading.Timer(10, Clear_Page).start()
     return HttpResponse(Patch_Cript)
 
+# Удаление ссылки на загрузку
 def Clear_Page():
     print("Очистка динамической ссылки для загрузки")
     global Patch_Of_Get
     Patch_Of_Get = "Non"
     return
 
+# Выдача данных из базы данных
 def Page_Return_Data(request, path):
     global Patch_Of_Get
     if path == Patch_Of_Get and Patch_Of_Get != "Non":
@@ -75,27 +73,4 @@ def Page_Return_Data(request, path):
         return Data_Base()
     return redirect("/index")
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
 
-def grecaptcha_verify(request):
-    if request.method == 'POST':
-        response = {}
-        data = request.POST
-        captcha_rs = data.get('g-recaptcha-response')
-        url = "https://www.google.com/recaptcha/api/siteverify"
-        params = {
-            'secret': settings.RECAPTCHA_SECRET_KEY,
-            'response': captcha_rs,
-            'remoteip': get_client_ip(request)
-        }
-        verify_rs = requests.get(url, params=params, verify=True)
-        verify_rs = verify_rs.json()
-        response["status"] = verify_rs.get("success", False)
-        response['message'] = verify_rs.get('error-codes', None) or "Unspecified error."
-        return HttpResponse(response)
